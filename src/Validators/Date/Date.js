27239c8c296730fbf0bc8382e2b7df9a6e769a86
Matthew Thomas <matthew.thomas@nhs.net>
2024-04-17 15:39:47 +0100

    make optional date work

--- src/Validators/Date/Date.js
+++ src/Validators/Date/Date.js
@@ -1,67 +1,86 @@
 import dayjs from "dayjs";
 
-export function Date(day, month, year, typeOfDate, validator) {
-    var fieldArr = [
-        day,
-        month,
-        year
-    ];  
-    validator.validators.push(
-        {
-            fieldName: day,
-            field: fieldArr,
-            rules: [{
-                method: function (field) {
-                    console.log(field);
-                    
-                    if(validator.form.elements[field[0]].value.length <= 0 || validator.form.elements[field[1]].value.length <= 0
-                                       || validator.form.elements[field[2]].value.length < 4){                     
-                        return ;
-                    }
+export function Date(day, month, year, typeOfDate, required = true, validator) {
+  var fieldArr = [
+    day,
+    month,
+    year
+  ];
+  validator.validators.push(
+    {
+      fieldName: day,
+      field: fieldArr,
+      rules: [{
+        method: function (field) {
 
-                    var dValue = validator.form.elements[field[0]].value.padStart(2, '0');
-                    var mValue = validator.form.elements[field[1]].value.padStart(2, '0');
-                    var yValue = validator.form.elements[field[2]].value;
-                    var d = yValue.concat('-', mValue, '-', dValue);
-                    var d2 = [yValue, mValue, dValue].join('-');
-                    var valid = dayjs(d2, 'YYYY-MM-DD').format('YYYY-MM-DD') === d2;
-                   
-                    return valid;
-                },                   
-                
-                message: "The date of "+typeOfDate+" supplied is an invalid date"
-            }]
-        });   
+          function validateDate() {
+            console.log('in ValidateDate');
+            var dValue = validator.form.elements[day].value.padStart(2, '0');
+            var mValue = validator.form.elements[month].value.padStart(2, '0');
+            var yValue = validator.form.elements[year].value;
+            var d = yValue.concat('-', mValue, '-', dValue);
+            var d2 = [yValue, mValue, dValue].join('-');
+            var valid = dayjs(d2, 'YYYY-MM-DD').format('YYYY-MM-DD') === d2;
+            return valid;
+          }
 
+          function isDirty() {
+            console.log('isDirty')
+            var d = validator.form.elements[day].value.length > 0;
+            var m = validator.form.elements[month].value.length > 0;
+            var y = validator.form.elements[year].value.length > 0;
+            console.log(d + m + y);
+            return d + m + y;
+          }
 
-    validator.addValidator(day, [{
-        method: function(field, params) {
-          return (params.day.value.trim().length > 0 && params.month.value.trim().length > 0 && params.year.value.trim().length === 4);
+          var isDirty = isDirty();
+          if (required) {
+            console.log('required is true')
+            return validateDate();
+          }
+          else if (isDirty > 0) {
+            console.log('date dirty so validate');
+            return validateDate();
+          }
+          else {
+            console.log('required is false');
+            return true;
+          }
         },
-        message: "Please enter a valid date of "+typeOfDate,
-        params: {
-          day: document.getElementById(day),
-          month: document.getElementById(month),
-          year: document.getElementById(year)
-        }
-      }]);
 
-    //   validator.addValidator(day, [{
-    //     method: function (field) {
-    //         return field.value.trim().length > 0;
-    //     },
-    //     message: 'No day has been supplied.'
-    //    }]);
-    // validator.addValidator(month, [{
-    //     method: function (field) {
-    //         return field.value.trim().length > 0;
-    //     },
-    //     message: 'No month has been supplied.'
-    // }]);
-    // validator.addValidator(year, [{
-    //     method: function (field) {
-    //         return field.value.trim().length === 4;
-    //     },
-    //     message: 'No year has been supplied'
-    // }]);
+        message: "The date of " + typeOfDate + " supplied is an invalid date"
+      }]
+    });
+
+
+  // validator.addValidator(day, [{
+  //     method: function(field, params) {
+  //       return (params.day.value.trim().length > 0 && params.month.value.trim().length > 0 && params.year.value.trim().length === 4);
+  //     },
+  //     message: "Please enter a valid date of "+typeOfDate,
+  //     params: {
+  //       day: document.getElementById(day),
+  //       month: document.getElementById(month),
+  //       year: document.getElementById(year)
+  //     }
+  //   }]);
+
+  //   validator.addValidator(day, [{
+  //     method: function (field) {
+  //         return field.value.trim().length > 0;
+  //     },
+  //     message: 'No day has been supplied.'
+  //    }]);
+  // validator.addValidator(month, [{
+  //     method: function (field) {
+  //         return field.value.trim().length > 0;
+  //     },
+  //     message: 'No month has been supplied.'
+  // }]);
+  // validator.addValidator(year, [{
+  //     method: function (field) {
+  //         return field.value.trim().length === 4;
+  //     },
+  //     message: 'No year has been supplied'
+  // }]);
 }
